dc2747793395d3461cc9388d98a9981d51a10b91
olewna <dajcie.suba2002@gmail.com>
2024-02-27 12:18:14 +0100

    added router to login page

--- frontend/src/app/core/layouts/navbar/navbar.component.ts
+++ frontend/src/app/core/layouts/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,15 +8,14 @@ import { AuthService } from '../../../shared/services/auth.service';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  constructor(private AuthService: AuthService) {}
-
-  public checkIfLogged(): void {
-    this.AuthService.isLogged();
-  }
-
+  constructor(private AuthService: AuthService, private router: Router) {}
   protected navbarCollapsed: boolean = true;
-
   public toggleNavbarCollapsing(): void {
     this.navbarCollapsed = !this.navbarCollapsed;
+  }
+
+  public goToLogin(): void {
+    this.router.navigate(['login']);
+    this.navbarCollapsed = true;
   }
 }

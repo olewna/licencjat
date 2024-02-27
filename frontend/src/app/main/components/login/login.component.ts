a55d098882dda2552fc3303b92dc28dab37e1a26
olewna <dajcie.suba2002@gmail.com>
2024-02-27 12:27:58 +0100

    changed name of the app and made login and register component

--- frontend/src/app/main/components/login/login.component.ts
+++ frontend/src/app/main/components/login/login.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  protected loginMode: boolean = true;
 
+  public changeMode(): void {
+    this.loginMode = !this.loginMode;
+  }
 }

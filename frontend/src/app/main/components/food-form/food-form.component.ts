d792f11e5150078474cc209da94dbe4bc08a3814
olewna <dajcie.suba2002@gmail.com>
2024-03-30 19:00:07 +0100

    added uploading for photos with preview

--- frontend/src/app/main/components/food-form/food-form.component.ts
+++ frontend/src/app/main/components/food-form/food-form.component.ts
@@ -1,16 +1,69 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import * as LR from '@uploadcare/blocks';
 
 @Component({
   selector: 'app-food-form',
   templateUrl: './food-form.component.html',
   styleUrls: ['./food-form.component.scss'],
 })
-export class FoodFormComponent {
-  protected files: any[] = [];
+export class FoodFormComponent implements OnInit, OnDestroy {
   protected owner: boolean = false;
+  protected url: string = '';
 
-  public handleUploaderEvent(e: Event) {
-    const { data: files } = (e as CustomEvent).detail;
-    this.files = files;
+  uploadedFiles: LR.OutputFileEntry<'success'>[] = [];
+  @ViewChild('ctxProvider', { static: true }) ctxProviderRef!: ElementRef<
+    InstanceType<LR.UploadCtxProvider>
+  >;
+
+  ngOnInit() {
+    this.ctxProviderRef.nativeElement.addEventListener(
+      'change',
+      this.handleChangeEvent
+    );
+
+    this.ctxProviderRef.nativeElement.addEventListener(
+      'modal-close',
+      this.handleModalCloseEvent
+    );
   }
+
+  ngOnDestroy() {
+    this.ctxProviderRef.nativeElement.removeEventListener(
+      'change',
+      this.handleChangeEvent
+    );
+
+    this.ctxProviderRef.nativeElement.removeEventListener(
+      'modal-close',
+      this.handleModalCloseEvent
+    );
+  }
+
+  resetUploaderState() {
+    this.ctxProviderRef.nativeElement.uploadCollection.clearAll();
+  }
+
+  handleModalCloseEvent = () => {
+    this.resetUploaderState();
+    this.uploadedFiles = [];
+    this.url = '';
+  };
+
+  handleChangeEvent = (e: LR.EventMap['change']) => {
+    this.uploadedFiles = e.detail.allEntries.filter(
+      (f) => f.status === 'success'
+    ) as LR.OutputFileEntry<'success'>[];
+    if (this.uploadedFiles.length > 0) {
+      this.url = this.uploadedFiles[0].cdnUrl;
+    }
+  };
 }

9f12b27f23ca7c5f6720244e65f1f7817ffd846c
olewna <dajcie.suba2002@gmail.com>
2024-04-13 15:29:23 +0200

    small fixes and music form component

--- frontend/src/app/main/components/music-form/music-form.component.ts
+++ frontend/src/app/main/components/music-form/music-form.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { MusicForm, Service } from 'src/app/shared/form.models/MusicForm.model';
+import { Music } from 'src/app/shared/models/Music.model';
+import { ComboService } from 'src/app/shared/services/combo.service';
 
 @Component({
   selector: 'app-music-form',
   templateUrl: './music-form.component.html',
-  styleUrls: ['./music-form.component.scss']
+  styleUrls: ['./music-form.component.scss'],
 })
-export class MusicFormComponent {
+export class MusicFormComponent implements OnInit {
+  public constructor(
+    private formbuilder: NonNullableFormBuilder,
+    private router: Router,
+    private location: Location,
+    private comboService: ComboService
+  ) {}
+  protected musicForm!: FormGroup<MusicForm>;
+  protected services: string[] = ['spotify', 'youtube', 'soundcloud', 'other'];
 
+  public ngOnInit(): void {
+    this.musicForm = this.formbuilder.group({
+      name: ['', [Validators.required]],
+      type: ['', [Validators.required]],
+      author: ['', [Validators.required]],
+      length: ['', [Validators.required]],
+      service: new FormArray<FormGroup<Service>>([]),
+      image: [''],
+    });
+
+    this.services.forEach((x: string) => {
+      const newItem: FormGroup<Service> = this.formbuilder.group({
+        name: [x],
+        checked: [false],
+      });
+      this.musicForm.controls.service.push(newItem);
+    });
+  }
+
+  protected getServices(): FormArray<FormGroup<Service>> {
+    return this.musicForm.controls.service as FormArray<FormGroup<Service>>;
+  }
+
+  public onSubmit(): void {
+    console.log(this.musicForm.value);
+    // this.comboService.addGame(this.musicForm.value as Music).subscribe({
+    //   next: (val: Music) => {
+    //     this.musicForm.reset();
+    //   },
+    //   error: (err: HttpErrorResponse) => {
+    //     console.log(err.error.message);
+    //   },
+    // });
+  }
+
+  public getImageUrl(): string {
+    return this.musicForm.get('image')!.value;
+  }
+
+  protected setImageUrl(url: string): void {
+    this.musicForm.get('image')?.setValue(url);
+  }
+
+  public deleteImage(): void {
+    this.musicForm.get('image')?.setValue('');
+  }
+
+  public goBack(): void {
+    this.location.back();
+  }
+
+  public goToFood(): void {
+    this.router.navigate(['food', 'form']);
+  }
+
+  public goToGames(): void {
+    this.router.navigate(['games', 'form']);
+  }
 }
---
+++ frontend/src/app/shared/form.models/MusicForm.model.ts
@@ -0,0 +1,24 @@
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+
+export interface MusicForm {
+  readonly name: FormControl<string>;
+  readonly type: FormControl<string>;
+  readonly length: FormControl<string>;
+  readonly author: FormControl<string>;
+  readonly service: FormArray<FormGroup<Service>>;
+  readonly image: FormControl<string>;
+}
+
+export interface Service {
+  readonly name: FormControl<string>;
+  readonly checked: FormControl<boolean>;
+}
+
+export interface MusicRequest {
+  readonly name: string;
+  readonly type: string;
+  readonly length: string;
+  readonly author: string;
+  readonly service: string[];
+  readonly logoUrl: string;
+}
